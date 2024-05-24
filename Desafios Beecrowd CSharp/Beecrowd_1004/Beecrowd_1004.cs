8ea7bf4568f79689768c7d95c6ba4476dabad001
lucasacoelho <lucas.acoelho@hotmail.com>
2024-05-24 17:19:30 -0300

    mais alguns exercicios

---
+++ Desafios Beecrowd CSharp/Beecrowd_1004/Beecrowd_1004.cs
@@ -0,0 +1,11 @@
+﻿namespace Desafios_Beecrowd_CSharp;
+
+static class Beecrowd_100
+{
+    private static void Main(string[] args)
+    {
+        int A = Convert.ToInt32(Console.ReadLine());
+        int B = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine($"PROD = {A * B}");
+    }
+}
